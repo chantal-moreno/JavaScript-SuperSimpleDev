4fe4ea015c58e2a9712743bb69dd848d6a903702
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-01-10 14:00:24 -0600

    Lesson 11 Code: Arrays and Loops Part 1

---
+++ Lesson11/index.js
@@ -0,0 +1,22 @@
+const todoList = ['make dinner', 'wash dishes'];
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  return console.log(todoListHTML);
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  todoList.push(name);
+  inputElement.value = '';
+  renderTodoList();
+  return console.log(todoList);
+}
