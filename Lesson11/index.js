0a778609ba96d90c217497873a430897b48103d4
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-01-12 18:25:05 -0600

    Lesson 11 Exercises: Arrays and Loops Part 2

--- Lesson11/index.js
+++ Lesson11/index.js
@@ -1,16 +1,9 @@
-const todoList = [
-  {
-    name: 'make dinner',
-    dueDate: '2024-01-12',
-  },
-  {
-    name: 'wash dishes',
-    dueDate: '2024-01-12',
-  },
-];
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
 renderTodoList();
 
 function renderTodoList() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
   let todoListHTML = '';
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
@@ -158,3 +151,133 @@ function removeTodo(index) {
 //   }
 //   return console.log(words);
 // }
+
+//Exercises Part 2
+// 11o. Create an array of strings, loop over the array, and check if the string 'search' is inside the array. If it is, console.log() the index of search in the array. If not, console.log '-1'.
+// const strings1 = ['hello', 'world', 'search', 'good'];
+// const strings2 = ['not', 'found'];
+// let index = -1;
+// for (let i = 0; i < strings2.length; i++) {
+//   if (strings2[i] === 'search') {
+//     index = i;
+//   }
+// }
+// console.log(index);
+// 11p. Modify 11o so that if 'search' appears multiple times in the array, it will console.log() the index of the first appearance of 'search'. Use break;
+const strings1 = ['hello', 'world', 'search', 'good', 'search'];
+let index = -1;
+for (let i = 0; i < strings1.length; i++) {
+  if (strings1[i] === 'search') {
+    index = i;
+    break;
+  }
+}
+console.log(index);
+// 11q. Create a function findIndex(array, word) that searches an array for a string (in the 'word' parameter) and return the index of the firts appearance of the string. If it doesn´t exist in the array, return -1.
+// findIndex(['green', 'red', 'blue', 'red'], 'red');
+// findIndex(['green', 'red', 'blue', 'red'], 'yellow');
+// function findIndex(array, word) {
+//   let index = -1;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === word) {
+//       index = i;
+//       break;
+//     }
+//   }
+//   return console.log(index);
+// }
+// 11r. Create a function removeEgg(foods) that takes an array of strings and returns an array where the string 'egg' is removed.
+// removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']);
+// function removeEgg(foods) {
+//   const result = [];
+//   for (let i = 0; i < foods.length; i++) {
+//     if (foods[i] === 'egg') {
+//       continue;
+//     } else {
+//       result.push(foods[i]);
+//     }
+//   }
+//   return console.log(result);
+// }
+// 11s. Update exercise 11r to only remove the first 2 eggs from the array.
+// removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']);
+// function removeEgg(foods) {
+//   const result = [];
+//   let eggsRemoved = 0;
+//   for (let i = 0; i < foods.length; i++) {
+//     if (foods[i] === 'egg' && eggsRemoved < 2) {
+//       eggsRemoved++;
+//       continue;
+//     } else {
+//       result.push(foods[i]);
+//     }
+//   }
+//   return console.log(result);
+// }
+// 11t. Arrays have a methos called .reverse(), which reverses the order of the values in the array. Using .reverse(), update exercise 11s to only remove the last 2 eggs from the array.
+// removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']);
+// function removeEgg(foods) {
+//   const reversedFoods = foods.reverse();
+//   const result = [];
+//   let eggsRemoved = 0;
+//   for (let i = 0; i < reversedFoods.length; i++) {
+//     if (reversedFoods[i] === 'egg' && eggsRemoved < 2) {
+//       eggsRemoved++;
+//       continue;
+//     } else {
+//       result.push(reversedFoods[i]);
+//     }
+//   }
+//   return console.log(result.reverse());
+// }
+// 11u. In exercise 11t, one problem with .reverse() is that it doesn´t create a copy of the array it is reversing. It modifies the original array. Update the code so the function does not modify the original array.
+removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']);
+function removeEgg(foods) {
+  const reversedFoods = foods.slice().reverse();
+  const result = [];
+  let eggsRemoved = 0;
+  for (let i = 0; i < reversedFoods.length; i++) {
+    if (reversedFoods[i] === 'egg' && eggsRemoved < 2) {
+      eggsRemoved++;
+      continue;
+    } else {
+      result.push(reversedFoods[i]);
+    }
+  }
+  return console.log(result.reverse(), foods);
+}
+// 11v. We'll do the famous FizzBuzz problem. Create a loop that displays 1 to 20 in the console. If the number is divisible by 3, display 'Fizz' instead of the number. If it's divisible by 5, display 'Buzz' instead. If it´s divisible by 3 and 5, display 'FizzBuzz' instead.
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
+// 11w. Create a copy of exercise 11q, where we created findIndex(). Below it, create a function unique(array) that takes an array of strings and returns an array where each string only appears once (there are no duplicates)
+function findIndex(array, word) {
+  let index = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === word) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+unique(['green', 'red', 'blue', 'red']);
+unique(['red', 'green', 'green', 'red']);
+function unique(array) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (findIndex(result, array[i]) === -1) {
+      result.push(array[i]);
+    }
+  }
+  return console.log(result);
+}
+// 11x. Update the to-do list to use localStorage
